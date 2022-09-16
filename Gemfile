9ab673d24d1a0d4749c08f1188f2db694d0b3abd
Rell <me@rlxfly.my.id>
2022-09-16 21:26:17 +0700

    cuih

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in telegram_bot.gemspec
+gem 'telegram_bot'
+gem  'json'
+gem 'net-http-persistent', '~> 2.9', '>= 2.9.4'
+gem 'rubocop', require: false
---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative './example/bot.rb'
+
+puts "Hello! This is beta Telegram bot by Re7~"
+puts "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+puts "This is a bot to help you stay motived by giving you motivational quotes"
+puts "This bot can also keep you entertained by providing you with jokes"
+puts "This bot will help you when you feeling down and demotived"
+sleep(0.5)
+puts "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+puts "Loading..."
+puts "The bot is now online!"
+puts "Use CTRL + C to stop the bot~"
