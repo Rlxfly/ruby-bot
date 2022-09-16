a94afa657f236a1950bb9409b5da8675775b1810
Rell <me@rlxfly.my.id>
2022-09-16 21:42:43 +0700

    cuih

---
+++ lib/telegram_bot/api_response.rb
@@ -0,0 +1,30 @@
+module TelegramBot
+    class ApiResponse < Result
+        class ResponseError < StandardError
+            attr_reader :response 
+
+            def initialize(res)
+                @response = res
+            end
+
+            def data
+                @data ||= begin
+                    JSON.parse(response.body)
+                rescue JSON::ParserError
+                    { error_code: response.status, error_message: response_phrase }
+                end
+            end
+        end
+
+        def self.from_excon(res)
+            body = res.body
+            if res.status == 200
+                data = JSON.parse(body)
+                new(data["result"], nil)
+            else
+                error = ResponseError.new(res)
+                new(nil, error)
+            end
+        end
+    end
+end
---
+++ lib/telegram_bot/bot.rb
@@ -0,0 +1,92 @@
+module TelegramBot
+    class WebhookRequest
+        include Virtus.model 
+        attribute :url, String
+        attribute :allowed_updates. [String]
+    end
+
+    class UpdateRequest
+        include Virtus.model
+        attribute :offset, Integer
+        attribute :timeout, Integer
+
+        def to_h
+            result = super.select { |_, v| !!v }
+            Hash[result]
+        end
+    end
+
+    class Bot
+        def initialize(opts = {})
+            @offset = opts[:offset] || 0
+            @logger = opts.fetch(:logger)
+            @client = opts.fetch(:client)
+        end
+
+        def get_me
+            @me ||= @client
+            .request(:getMe)
+            .and_then do |result|
+                User.new(result)
+            end
+            .value!
+        end
+        alias_method :me, :get_me
+        alias_method :identity, :me
+
+        def get_updates(opts = {}, &block)
+            return get_last_messages(opts) unless block_given?
+
+            opts[:timeout] ||= 50
+            logger.info "Starting get_updates loop..."
+            loop do 
+                messages = get_last_messages(opts)
+                messages.compact.each do |message|
+                    next unless message
+                    logger.info "Message from #{message.chat.friendly_name}: #{message.text.inspect}"
+                    yield message
+                end
+            end
+        end
+
+        def send_message(out_message)
+            logger.info "Sending message: #{out_message.text.inspect} to #{out_message.chat_friendly_name}"
+            @client
+            .request(:sendMessage, out_message)
+            .and_then do |result|
+                Message.new(result)
+            end
+            .value!
+        end
+
+        def set_webhook(url, allowed_updates: %i(message))
+            logger.info "Setting webhook url to #{url}, allowed_updates #{alloweb_updates}"
+            request = WebhookRequest.new(url: url, allowed_updates)
+            @client.request(:setWebhook, request)
+        end
+
+        def remove_webhook
+            set_webhook("")
+        end
+
+        private
+        attr_reader :logger
+
+        def get_last_updates(opts = {})
+            opts[:offset] ||= @offset
+            request = UpdateRequest.new(opts)
+            response = @client.request(:getUpdates, request)
+            if opts[:fail_silently] && !response.ok?
+                logger.warn "Error when getting updates. Ignoring due to fail_silently"
+                return []
+            end
+            updates = response.value!.compact.map{ |raw_update| Update.new(raw_update) }
+            @offset = updates.last.id + 1 if updates.any?
+            updates
+        end
+
+        def get_last_messages(opts = {})
+            get_last_updates(opts).map(&:message)
+        end
+    end
+end
